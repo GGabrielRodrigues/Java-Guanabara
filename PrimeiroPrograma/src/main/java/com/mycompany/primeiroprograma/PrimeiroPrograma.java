1c0b29094e9853a63ee19da37e394e0229d7de56
GGabrielRodrigues <gabriel.rodrigues2345@discente.ufg.br>
2025-03-12 07:43:11 -0300

    Primeiro commit

---
+++ PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
