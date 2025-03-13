7586dae6e4abc585ba1f15886cb52e517fab71e0
GGabrielRodrigues <gabriel.rodrigues2345@discente.ufg.br>
2025-03-13 14:17:33 -0300

    Tipos primitivos em Java

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -14,9 +14,35 @@ public class TiposPrimitivos {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        String nome = leitor.nextLine();
-        float nota = leitor.nextFloat();
-        System.out.printf("A nota de %s é: %.2f", nome, nota);
+        System.out.printf("É casado? (true ou false): ");
+        boolean casado = leitor.nextBoolean();
+        leitor.nextLine();
+        System.out.print("Insira o caracter: ");
+        char caractere = leitor.next().charAt(0);
+        leitor.nextLine();
+        System.out.print("Insira a sua nacionalidade: ");
+        String nacionalidade = leitor.nextLine();
+        System.out.print("Insira a sua idade: ");
+        byte idade = leitor.nextByte();
+        System.out.print("Insira o seu DDD: ");
+        short DDD = leitor.nextShort();
+        System.out.print("Insira o seu ano de nascimento: ");
+        int ano = leitor.nextInt();
+        System.out.print("Insira o seu CPF: ");
+        long CPF = leitor.nextLong();
+        System.out.print("Insira a sua altura (cm): ");
+        float altura = leitor.nextFloat();
+        System.out.print("Comprimento da unha (mm): ");
+        double unha = leitor.nextDouble();
+        
+        //
+        System.out.println("\nCasado: "+ casado + "\nCaractere inserido: " + caractere + "\nNacionalidade: " + nacionalidade);
+        System.out.println("Idade: " + idade + "\nDDD: " + DDD + "\nAno: " + ano + "\nCPF: " + CPF + "\nAltura: " + altura);
+        System.out.println("Comprimento da unha: " + unha);
+        
+        
+        
+       
     }
     
 }
