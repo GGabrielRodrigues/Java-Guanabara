afd3e090fe99f872135b333b14f9d58f714cbee0
GGabrielRodrigues <gabriel.rodrigues2345@discente.ufg.br>
2025-03-14 15:03:17 -0300

    SomaSwing

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author gabri
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        
+        System.out.println("Valor: " + valor);
+        
+    }
+    
+}
